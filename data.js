c12d34a408831bf51d574afa973c0ebc5633a929
debrabidasunofficial-a11y <debrabidasunofficial@gmail.com>
2026-06-04 06:50:26 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,72 @@
+// ==========================
+// STUDENTOS DATA
+// ==========================
 
+const studentData = {
+
+    name: "Vasu",
+
+    rank: "Shadow Knight",
+
+    academicHealth: 82,
+
+    forgeScore: 76,
+
+    recallScore: 69,
+
+    jeeDate: "2026-12-31",
+
+    subjects: [
+
+        {
+            name: "Physics",
+            color: "#3b82f6"
+        },
+
+        {
+            name: "Chemistry",
+            color: "#22c55e"
+        },
+
+        {
+            name: "Mathematics",
+            color: "#a855f7"
+        },
+
+        {
+            name: "Biology",
+            color: "#06b6d4"
+        },
+
+        {
+            name: "English",
+            color: "#f59e0b"
+        },
+
+        {
+            name: "Bengali",
+            color: "#fb7185"
+        }
+
+    ],
+
+    missions: [
+
+        {
+            task: "Physics NLM DPP",
+            completed: false
+        },
+
+        {
+            task: "Maths Functions Revision",
+            completed: false
+        },
+
+        {
+            task: "Chemistry Module",
+            completed: false
+        }
+
+    ]
+
+};
